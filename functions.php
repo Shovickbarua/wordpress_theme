1a89231172c6c2ed2626681a381cf4eb978b064d
Shovickbarua <shovickbarua92@gmail.com>
2024-08-31 17:22:12 +0600

    update

--- functions.php
+++ functions.php
@@ -11,6 +11,19 @@ if( !function_exists('wp_test') ) {
             function myscript(){
                 wp_enqueue_style( 'unique_id', get_stylesheet_uri(  ) );
                 wp_enqueue_style( 'main_css', get_parent_theme_file_uri( 'assets/css/main.css' ), array(), wp_get_theme()->get('Version'), 'all'  ) ;
+                // wp_add_inline_style(
+                //     'unique_id', 
+                //     'body{ background: red; }'
+                // );
+                wp_enqueue_script( 
+                    'main',
+                    get_template_directory_uri(  ) .'/assets/js/main.js',
+                    array(), 
+                    wp_get_theme()->get('Version'), 
+                    true
+                );
+
+                wp_add_inline_script('main', 'console.log("All data");',);
             }
         }
     }
--- index.php
+++ index.php
@@ -4,8 +4,15 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <?php
+        wp_head('top');   
+    ?>
 </head>
 <body>
-    
+    <h1>sajd</h1>
+    <img src="<?php echo esc_url(get_theme_file_uri( 'assets/images/samsung.jpg '))?>" >
+    <?php 
+        wp_footer('one') ;
+    ?>
 </body>
 </html>
